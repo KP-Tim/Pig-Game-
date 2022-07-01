48c967ddcd58767efe6dc5ae3a565760ece01c1b
KP-Tim <kancanha@gmail.com>
2022-07-01 08:53:25 -0700

    Pig Game Starting Files

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const player1Score = document.querySelector('#score--0');
+const player2Score = document.querySelector('#score--1');
+const diceImg = document.querySelector('dice');
+console.log(player1Score);
+console.log(player2Score);
+player1Score.textContent = '0';
+player2Score.textContent = '0';
