c2e54408e9da0cb790835d4ca1b3439f90324fc5
KP-Tim <kancanha@gmail.com>
2022-07-01 09:10:08 -0700

    scores to 0 and hide the dice

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const player1Score = document.querySelector('#score--0');
-const player2Score = document.querySelector('#score--1');
-const diceImg = document.querySelector('dice');
-console.log(player1Score);
-console.log(player2Score);
-player1Score.textContent = '0';
-player2Score.textContent = '0';
+// Selecting elements
+const player1ScoreEl = document.querySelector('#score--0');
+const player2ScoreEl = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+player1ScoreEl.textContent = 0;
+player2ScoreEl.textContent = 0;
+
+diceEl.classList.add('hidden');
